70b7207005a75e1a37766a1ed1bfe64e24347739
TaghreedNageeb <50971460+TaghreedNageeb@users.noreply.github.com>
2019-12-16 15:09:05 +0300

    adding all files

---
+++ build/precache-manifest.8a53d6ce42ed5cda15e81e8bbe787962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15fc350ee5454dfcd9f34b7de0aa07b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5e182e27508efc7f84",
+    "url": "/static/css/main.550d1fb6.chunk.css"
+  },
+  {
+    "revision": "c3e2c912770e41b25243",
+    "url": "/static/js/2.65989a3f.chunk.js"
+  },
+  {
+    "revision": "fb5e182e27508efc7f84",
+    "url": "/static/js/main.e18dabf1.chunk.js"
+  },
+  {
+    "revision": "2ffaf4ba443da19904ad",
+    "url": "/static/js/runtime-main.f1fe54bf.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <div>       
+      </div>
+    );
+  }
+}
+
+export default App;
